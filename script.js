1dcd0814b2c96b86eb1f8128b0f0047e5670bbb2
vishalsai-cmd <rvishalsai2005@gmail.com>
2023-06-12 19:50:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/* var rows=3;
+var columns=3;
+var currtile;
+var othertile;
+var imgOrder=["1","2","3","4","5","6","7","8","9"]
+var imgOrder1=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]
+var imgOrder2=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"]
+for(let r=0;r<rows;r++)
+{
+     for(let c=0;c<columns;c++)
+     {
+         let tile=document.createElement("img");
+         tile.id=r.toString()+""+c.toString()
+         tile.src=imgOrder.shift()+".jpg"
+         console.log(tile.src)
+        }
+    } */
+/* var image=document.getElementById("image");
+let inputvalue=image.Value
+document.getElementById("output").innerHTML=inputvalue; */
+
